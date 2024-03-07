8bb3efe6ce60385ad9475a5a9d729a23bbb058f6
HP <marcelonuigbo415@gmail.com>
2024-03-07 15:02:04 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿namespace MyATMApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyBank bk = new MyBank();
+
+            bool condition = true;
+
+            while (condition)
+            {
+                Console.WriteLine("1. Check account Balance");
+                Console.WriteLine("2. Deposit Funds");
+                Console.WriteLine("3. Withdraw Cash");
+                Console.WriteLine("4. Transfer Money");
+
+                // Read the user's choice
+                string input = Console.ReadLine();
+                int choice = Convert.ToInt32(input);
+
+                switch (choice)
+                {
+                    case 1:
+                        bk.checkBalance();
+                        break;
+                    case 2:
+                        bk.depositFunds();
+                        break;
+                    case 3:
+                        bk.withdraw();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice");
+                        break;
+                }
+            }
+        }
+    }
+
+    }
+
+
